5b492834f72b50f53854dbe748f2b617cd684332
UsseF-1 <youseefahmed425@gmail.com>
2026-06-23 17:23:36 +0300

    edit on eks file & add role for ebs

--- terraform/ekscluster.tf
+++ terraform/ekscluster.tf
@@ -5,10 +5,8 @@ module "eks" {
     cluster_name    = var.eks_name
     cluster_version = "1.30"
 
-    # Makes the cluster API accessible from your laptop
     cluster_endpoint_public_access = true
 
-    # Gives the person running terraform admin access automatically
     enable_cluster_creator_admin_permissions = true
 
     vpc_id     = module.vpc.vpc_id
@@ -16,21 +14,22 @@ module "eks" {
 
     cluster_addons = {
         aws-ebs-csi-driver = {
-            most_recent = true
+        most_recent              = true
+        service_account_role_arn = module.ebs_csi_irsa_role.iam_role_arn
         }
     }
 
-    # This defines the actual servers that run your pods
     eks_managed_node_groups = {
         general = {
-            instance_types = ["t3.medium"]
-            min_size       = 1
-            max_size       = 2
-            desired_size   = 1
+        instance_types = ["t3.medium"]
+
+        min_size     = 1
+        max_size     = 2
+        desired_size = 1
         }
-}
+    }
 
     tags = {
-        Name        = "GitOps-EKS-Cluster"
+        Name = "GitOps-EKS-Cluster"
     }
 }
