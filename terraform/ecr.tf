cc4a30672724c0de39707a0d489ddead16f79a11
UsseF-1 <youseefahmed425@gmail.com>
2026-06-24 15:24:20 +0300

    Edit on ECR to not delete images after destroy

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,47 +1,54 @@
 resource "aws_ecr_repository" "app" {
-    name = "vprofile-app"
-
+    name                 = "vprofile-app"
     image_tag_mutability = "MUTABLE"
 
     image_scanning_configuration {
         scan_on_push = true
     }
 
-    force_delete = true
+    force_delete = false
+
+    lifecycle {
+        prevent_destroy = true
+    }
 
     tags = {
         Name = "vprofile-app"
     }
-}
+    }
 
-
-resource "aws_ecr_repository" "db" {
-    name = "vprofile-db"
-
+    resource "aws_ecr_repository" "db" {
+    name                 = "vprofile-db"
     image_tag_mutability = "MUTABLE"
 
     image_scanning_configuration {
         scan_on_push = true
     }
 
-    force_delete = true
+    force_delete = false
+
+    lifecycle {
+        prevent_destroy = true
+    }
 
     tags = {
         Name = "vprofile-db"
     }
-}
+    }
 
-
-resource "aws_ecr_repository" "web" {
-    name = "vprofile-web"
-
+    resource "aws_ecr_repository" "web" {
+    name                 = "vprofile-web"
     image_tag_mutability = "MUTABLE"
 
     image_scanning_configuration {
         scan_on_push = true
     }
 
-    force_delete = true
+    force_delete = false
+
+    lifecycle {
+        prevent_destroy = true
+    }
 
     tags = {
         Name = "vprofile-web"
