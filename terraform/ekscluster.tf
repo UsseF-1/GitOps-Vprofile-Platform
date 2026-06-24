50df1e3259621433c1fe0c851de3b23c09c8a94e
UsseF-1 <youseefahmed425@gmail.com>
2026-06-24 16:46:42 +0300

    add new node to EKS

--- terraform/ekscluster.tf
+++ terraform/ekscluster.tf
@@ -23,9 +23,9 @@ module "eks" {
         general = {
         instance_types = ["t3.medium"]
 
-        min_size     = 1
-        max_size     = 2
-        desired_size = 1
+        desired_size = 2
+        min_size     = 2
+        max_size     = 3
         }
     }
 
